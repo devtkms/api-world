be1d43229c527750c52eb0bc434ce96dd4f095c8
Takumi Suzuki <devtkmszk824@gmail.com>
2024-11-03 08:30:43 +0900

    Add ReflectionController for retrieving class info
    Implemented a new `ReflectionController` with an endpoint to retrieve class information using reflection. The controller interacts with `ReflectionService` and returns the class details wrapped in `ApiResponseDto`.

---
+++ src/main/java/com/devtkms/apiworld/reflection/controller/ReflectionController.java
@@ -0,0 +1,32 @@
+package com.devtkms.apiworld.reflection.controller;
+
+import com.devtkms.apiworld.common.dto.ApiResponseDto;
+import com.devtkms.apiworld.reflection.dto.ClassInfoDto;
+import com.devtkms.apiworld.reflection.service.ReflectionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Controller for retrieving class information using reflection.
+ */
+@RestController
+@RequestMapping("/api/reflection")
+public class ReflectionController {
+
+    @Autowired
+    private ReflectionService reflectionService;
+
+    /**
+     * Retrieves class information for the specified class name.
+     *
+     * @param className the name of the class to retrieve information for
+     * @return ResponseEntity containing the class information wrapped in ApiResponseDto
+     */
+    @GetMapping("/class-info/{className}")
+    public ResponseEntity<ApiResponseDto<ClassInfoDto>> getClassInfo(@PathVariable String className) {
+        ClassInfoDto classInfo = reflectionService.getClassInfo(className);
+        return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+}
